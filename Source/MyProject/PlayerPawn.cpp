4ccb44cd32c5b0692bc888282d66f533d60286cf
Daniel Olsson <125807379+OfficialBabyboidaniel@users.noreply.github.com>
2023-05-07 13:19:47 +0200

    l

--- Source/MyProject/PlayerPawn.cpp
+++ Source/MyProject/PlayerPawn.cpp
@@ -40,6 +40,7 @@ APlayerPawn::APlayerPawn()
 void APlayerPawn::BeginPlay()
 {
 	Super::BeginPlay();
+	
 }
 
 // Called every frame
@@ -61,17 +62,25 @@ void APlayerPawn::Tick(float DeltaTime)
 	Velocity.Y = 0;
 	SetActorLocation(GetActorLocation() + Velocity * DeltaTime);
 	JumpMovement = FVector(0);*/
-
+	
+	
+	DrawDebugBox(GetWorld(), Origin, Extent, FColor::Red, true);
+	
 	const double Distance = MovementSpeed * DeltaTime;
 	const FVector CurrentLocation = GetActorLocation();
 	FVector Movement = CurrentInput * Distance;
-	PreventCollision(DeltaTime, Movement);
-	if (!SweepHit) SetActorLocation(CurrentLocation + Movement);
+	//PreventCollision(DeltaTime, Movement);
+	
+	 SetActorLocation(CurrentLocation + PreventCollision(DeltaTime, Movement));
+	if (SweepHit)
+	{
+		DrawDebugBox(GetWorld(), Hit.ImpactPoint, Extent, FColor::Red, true);
+	}
 	// kolla här 
-	if(SweepHit) SetActorLocation(CurrentLocation + Movement.GetSafeNormal() * Hit.Distance);
 }
 
 // Called to bind functionality to input
+
 void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
@@ -97,97 +106,29 @@ void APlayerPawn::VerticalInput(float AxisValue)
 // jump input
 void APlayerPawn::JumpInput()
 {
-	// check if grounded
-	// Check what is above
-	/*FHitResult GroundedHit;
-	FHitResult JumpHit;
-	FCollisionQueryParams QueryParams;
-	FCollisionQueryParams QueryParams2;
-	const FVector TraceEndDownwards = Origin + FVector::DownVector * (SkinWidth + GroundCheckDistance);
-	const FVector TraceEndUpwards = Origin + FVector::UpVector * JumpHeight;
-	bool CollisionCheck = Sweep(GroundedHit, Origin, TraceEndDownwards, QueryParams);
-
-	if (Sweep(GroundedHit, Origin, TraceEndDownwards, QueryParams))
-	{
-		if (Sweep(JumpHit, Origin, TraceEndUpwards, QueryParams2))
-			JumpMovement = FVector::UpVector * FMath::Min(JumpHeight, TraceEndUpwards.Distance(Origin, TraceEndUpwards));
 	
-		JumpMovement = FVector::UpVector * JumpHeight;
-		UE_LOG(LogTemp, Display, TEXT("Jumpmovment %f"), JumpMovement.Size());
-	}
-	UE_LOG(LogTemp, Display, TEXT("in jump "));*/
 }
 
 
-void APlayerPawn::PreventCollision(float DeltaTime, FVector Movement)
+FVector APlayerPawn::PreventCollision(float DeltaTime, FVector Movement)
 {
-	
 	FVector TraceStart = GetActorLocation();
-	FVector TraceEnd = GetActorLocation() + Movement;
+	FVector TraceEnd = Origin + Movement.GetSafeNormal2D() /** (Movement.Size() * SkinWidth)*/;
 	FCollisionQueryParams Params;
+	GetActorBounds(true, Origin, Extent);
 	SweepHit = Sweep(Hit, TraceStart, TraceEnd, Params);
-	UE_LOG(LogTemp, Warning, TEXT("bhit = %hhd"), SweepHit);
-
-
-	/*FHitResult Hit;
-	FVector TraceEnd = Origin + Velocity.GetSafeNormal() * Velocity.Size() * DeltaTime + SkinWidth * Velocity.GetSafeNormal();
-	FCollisionQueryParams QueryParams;
-	QueryParams.AddIgnoredActor(this);
-
-	bool bHit;
-
-	UE_LOG(LogTemp, Display, TEXT("in PreventCollision"));
-
-	bHit = Sweep(Hit, Origin, TraceEnd, QueryParams);
-
-	if (Velocity.Size() * DeltaTime <  SmallMovement)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("returning zero vector, small movement"));
-		Velocity = FVector::ZeroVector;
-	}
-	else if (recursiveCounter == 10)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Recursive LOOP: %f"), recursiveCounter);
-		recursiveCounter = 0;
-		Velocity = FVector::ZeroVector;
-	}
-	else if (bHit)
-	{
-		UE_LOG(LogTemp, Display, TEXT("Collision detected 1"));
-
-		FVector TraceStart = Hit.ImpactPoint - Hit.Normal * Hit.Distance;
-		TraceStart.Y -= Extent.Y;
-		FVector NormalTraceEnd = Hit.ImpactPoint;
-		NormalTraceEnd.Y -= Extent.Y;
-		FHitResult NormalHit;
-		FCollisionQueryParams NormalParams;
-		NormalParams.AddIgnoredActor(Hit.GetActor());
-		// second sweep
-		bHit = Sweep(NormalHit, TraceStart, NormalTraceEnd, NormalParams);
-		if (bHit)
-		{
-
-			Velocity = -(NormalHit.Normal * (NormalHit.Distance + SkinWidth));
-			UE_LOG(LogTemp, Display, TEXT("Return 2, %d"), recursiveCounter++);
-			return;
-		}
-
-		FVector NormalF = StaticHelperClass::DotProduct(Velocity, Hit.ImpactNormal);
-		Velocity += NormalF + Hit.ImpactNormal * SkinWidth;
-		UE_LOG(LogTemp, Display, TEXT("Finished, %d"), recursiveCounter++);
-		UE_LOG(LogTemp, Display, TEXT("looking for more collisions"));
-		Velocity = NormalF;
-	}
-	*/
+	DrawDebugSphere(GetWorld(), Hit.ImpactPoint, 10.f, 8, FColor::Red, true);
+	if (SweepHit) return Movement.GetSafeNormal2D() /** (Hit.Distance - SkinWidth)*/;
+	else
+		return Movement;
 }
 
 
 bool APlayerPawn::Sweep(FHitResult& HitResult, const FVector& Start, const FVector& Target,
                         FCollisionQueryParams& Params) const
 {
+	
 	Params.AddIgnoredActor(this);
-	FVector Origin, Extent;
-	GetActorBounds(true, Origin, Extent);
 	return GetWorld()->SweepSingleByChannel(
 		HitResult,
 		Start,
